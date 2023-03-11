6d9fbc1b79b35b689211d013c57c32e78a7e1c68
RaVi <77927691+RaViii1@users.noreply.github.com>
2023-03-11 16:59:34 +0100

    Update dane.sql

--- dane.sql
+++ dane.sql
@@ -2,6 +2,10 @@ INSERT INTO `mydb`.`uzytkownik` (`iduzytkownik`, `email`, `imie`, `nazwisko`) VA
 INSERT INTO `mydb`.`uzytkownik` (`iduzytkownik`, `email`, `imie`, `nazwisko`) VALUES (2, "baran@gmail.com", "Jakub", "Baran");
 INSERT INTO `mydb`.`uzytkownik` (`iduzytkownik`, `email`, `imie`, `nazwisko`) VALUES (3, "maister@gmail.com", "Mateusz", "Adamczyk");
 INSERT INTO `mydb`.`uzytkownik` (`iduzytkownik`, `email`, `imie`, `nazwisko`) VALUES (4, "piter@gmail.com", "Piotr", "Rumcajs");
+INSERT INTO `mydb`.`uzytkownik` (`iduzytkownik`, `email`, `imie`, `nazwisko`) VALUES (5, "tomek@gmail.com", "Tomek", "Domek");
+INSERT INTO `mydb`.`uzytkownik` (`iduzytkownik`, `email`, `imie`, `nazwisko`) VALUES (6, "adrian@gmail.com", "Adrian", "Pazur");
+INSERT INTO `mydb`.`uzytkownik` (`iduzytkownik`, `email`, `imie`, `nazwisko`) VALUES (7, "maciek@gmail.com", "Maciek", "Bialy");
+INSERT INTO `mydb`.`uzytkownik` (`iduzytkownik`, `email`, `imie`, `nazwisko`) VALUES (8, "framuga@gmail.com", "Marcin", "Framuga");
 
 INSERT INTO `mydb`.`stan_zamowienia` (`idstan_zamowienia`, `nazwa`) VALUES (1, "Do realizacji");
 INSERT INTO `mydb`.`stan_zamowienia` (`idstan_zamowienia`, `nazwa`) VALUES (2, "W trakcie realizacji");
@@ -25,6 +29,10 @@ INSERT INTO `mydb`.`stan_techniczny` (`idstan_techniczny`, `nazwa`) VALUES (3, "
 INSERT INTO `mydb`.`stan_techniczny` (`idstan_techniczny`, `nazwa`) VALUES (4, "Do przegladu");
 
 INSERT INTO `mydb`.`klient` (`idklient`, `iduzytkownik`) VALUES (1, 4);
+INSERT INTO `mydb`.`klient` (`idklient`, `iduzytkownik`) VALUES (2, 5);
+INSERT INTO `mydb`.`klient` (`idklient`, `iduzytkownik`) VALUES (3, 6);
+INSERT INTO `mydb`.`klient` (`idklient`, `iduzytkownik`) VALUES (4, 7);
+INSERT INTO `mydb`.`klient` (`idklient`, `iduzytkownik`) VALUES (5, 8);
 
 INSERT INTO `mydb`.`zamowienie`
 (`idzamowienie`,
@@ -38,3 +46,50 @@ INSERT INTO `mydb`.`zamowienie`
 VALUES
 (1, 1, 1, "Bychawa 69A", "Glusko Duze 1C", 500.00, "2023-03-08", 1568.0);
 
+INSERT INTO `mydb`.`zamowienie`
+(`idzamowienie`,
+`stan_zamowienia`,
+`klient`,
+`adres_bazowy`,
+`adres_docelowy`,
+`masa`,
+`data_zlozenia`,
+`cena`)
+VALUES
+(2, 5, 2, "Zakrzowek 74B", "Lublin Lipowa 1C", 800.00, "2023-03-11", 2000.0);
+
+INSERT INTO `mydb`.`zamowienie`
+(`idzamowienie`,
+`stan_zamowienia`,
+`klient`,
+`adres_bazowy`,
+`adres_docelowy`,
+`masa`,
+`data_zlozenia`,
+`cena`)
+VALUES
+(3, 4, 3, "Lublin 24B", "Lublin Muzyczna 23A", 300.00, "2023-03-01", 476.0);
+
+INSERT INTO `mydb`.`zamowienie`
+(`idzamowienie`,
+`stan_zamowienia`,
+`klient`,
+`adres_bazowy`,
+`adres_docelowy`,
+`masa`,
+`data_zlozenia`,
+`cena`)
+VALUES
+(4, 1, 4, "Lublin 123A", "Wysokie", 1000.00, "2023-03-12", 2500.0);
+
+INSERT INTO `mydb`.`zamowienie`
+(`idzamowienie`,
+`stan_zamowienia`,
+`klient`,
+`adres_bazowy`,
+`adres_docelowy`,
+`masa`,
+`data_zlozenia`,
+`cena`)
+VALUES
+(5, 1, 5, "Warszawa 32A", "Kraków Koszykarska 4", 1500.00, "2023-03-12", 3500.0);

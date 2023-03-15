47472cd66af72e14ce222d9ef6a81f0c7e0a4293
damymeister <77884237+damymeister@users.noreply.github.com>
2023-03-15 17:15:09 +0100

    Create Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -2,7 +2,11 @@ SELECT * FROM `mydb`. stanowisko ORDER BY nazwa;
 SELECT AVG(cena), MIN(cena), MAX(cena) FROM `mydb`. zamowienie;
 SELECT * FROM `mydb`. uzytkownik WHERE imie=`Piotr`;
 
-SELECT u.imie, u.nazwisko, p.stanowisko FROM `mydb` . harmonogram 
-LEFT JOIN `mydb`.pracownik p ON harmonogram.pracownik = p.idpracownik
-LEFT JOIN  `mydb`.uzytkownik u ON pracownik.iduzytkownik = u.iduzytkownik
+SELECT * FROM `mydb` . harmonogram 
+LEFT JOIN `mydb`. pracownik ON harmonogram.pracownik = pracownik.idpracownik
+LEFT JOIN  `mydb`.uzytkownik ON pracownik.iduzytkownik = uzytkownik.iduzytkownik
 WHERE data_od > '2023-03-10';
+
+
+SELECT * FROM `mydb`.`pracownik` LEFT JOIN `mydb`.`zamowienie` ON pracownik.idzamowienie = zamowienie.idzamowienie;
+SELECT * FROM `mydb`.`pracownik` LEFT JOIN `mydb`.`stanowisko` ON pracownik.stanowisko = stanowisko.idstanowisko LEFT JOIN `mydb`.`uzytkownik`  ON uzytkownik.iduzytkownik = pracownik.iduzytkownik

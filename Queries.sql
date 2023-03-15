aaa11ec7b5040a6c2a55adda5b3bbfb69f24ec75
RaVi <77927691+RaViii1@users.noreply.github.com>
2023-03-15 17:11:35 +0100

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -1,4 +1,8 @@
 SELECT * FROM `mydb`. stanowisko ORDER BY nazwa;
 SELECT AVG(cena), MIN(cena), MAX(cena) FROM `mydb`. zamowienie;
 SELECT * FROM `mydb`. uzytkownik WHERE imie=`Piotr`;
-SELECT * FROM `mydb` . harmonogram LEFT JOIN `mydb`. pracownik ON harmonogram.pracownik = pracownik.idpracownik WHERE data_od > '2023-03-10';
+
+SELECT * FROM `mydb` . harmonogram 
+LEFT JOIN `mydb`. pracownik ON harmonogram.pracownik = pracownik.idpracownik
+LEFT JOIN  `mydb`.uzytkownik ON pracownik.iduzytkownik = uzytkownik.iduzytkownik
+WHERE data_od > '2023-03-10';

0fda0229e62e074b5bababa4343d17064f91df79
DingoMC <95879619+DingoMC@users.noreply.github.com>
2023-03-29 16:47:26 +0200

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -12,7 +12,7 @@ SELECT u.imie, u.nazwisko, s.nazwa, harmonogram.data_od, harmonogram.data_do FRO
 LEFT JOIN `mydb`.pracownik p ON harmonogram.pracownik = p.idpracownik
 LEFT JOIN `mydb`.uzytkownik u ON p.iduzytkownik = u.iduzytkownik
 LEFT JOIN `mydb`.stanowisko s ON p.stanowisko = s.idstanowisko
-WHERE data_do < "2023-03-10"
+WHERE data_do < "2023-03-10";
 
 #5 Wyswietlenia Imienia, nazwiska oraz id stanowiska osób pracujacych przed 10 marca
 SELECT u.imie, u.nazwisko, p.stanowisko FROM `mydb`.harmonogram 
@@ -29,19 +29,19 @@ ON pracownik.idzamowienie = zamowienie.idzamowienie;
 SELECT * FROM `mydb`.`pracownik` LEFT JOIN `mydb`.`stanowisko` 
 ON pracownik.stanowisko = stanowisko.idstanowisko LEFT JOIN `mydb`.`uzytkownik` 
 ON uzytkownik.iduzytkownik = pracownik.iduzytkownik
-WHERE idzamowienie IS NOT NULL
+WHERE idzamowienie IS NOT NULL;
 
 #8 Wyświetlenie stanu technicznego pojazdu, jego danych oraz statusu dostępności.
 SELECT stan_techniczny.nazwa, p.idpojazd, p.max_ladunek , p.dostepnosc FROM `mydb`. stan_techniczny
-LEFT JOIN `mydb`. pojazd p ON stan_techniczny.idstan_techniczny = p.stan_techniczny
+LEFT JOIN `mydb`. pojazd p ON stan_techniczny.idstan_techniczny = p.stan_techniczny;
 
 
 #9 Wyświetlenie id klienta, stanu zamówienia, adresu docelowego, ceny oraz id faktury złożonego zamówienia.
 SELECT klient.idklient, z.stan_zamowienia, z.adres_docelowy, z.cena, f.idfaktura FROM `mydb`. klient
 LEFT JOIN `mydb`. zamowienie z ON klient.idklient = z.klient
 LEFT JOIN `mydb`. faktura f ON z.idzamowienie = f.idzamowienie
-ORDER BY stan_zamowienia
+ORDER BY stan_zamowienia;
 
 #10 Wyświetlenie id pojazdu, maksymalnego ładunku oraz jego statusu dostepności.
 SELECT p.max_ladunek, dostepnosc_pojazdu.nazwa FROM `mydb`. dostepnosc_pojazdu
-LEFT JOIN `mydb`. pojazd p on dostepnosc_pojazdu.iddostepnosc_pojazdu = p.dostepnosc
+LEFT JOIN `mydb`. pojazd p on dostepnosc_pojazdu.iddostepnosc_pojazdu = p.dostepnosc;
